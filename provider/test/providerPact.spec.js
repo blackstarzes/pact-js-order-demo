028bab02fb031de8b4cabbed9a3417f6c9a0cadb
Matt Fellows <matt.fellows@onegeek.com.au>
2019-05-03 12:31:50 +1000

    refactor: remove provider states code for now

--- provider/test/providerPact.spec.js
+++ provider/test/providerPact.spec.js
@@ -4,20 +4,7 @@ const chai = require('chai')
 const chaiAsPromised = require('chai-as-promised')
 chai.use(chaiAsPromised)
 
-let { server, dataStore } = require('../provider.js')
-
-// Set the current state
-server.post('/setup', (req, res) => {
-  switch (req.body.state) {
-    case 'there are no orders':
-      dataStore = []
-      break
-    default:
-      dataStore = require('../data/orders')
-  }
-
-  res.end()
-})
+let { server } = require('../provider.js')
 
 server.listen(8081, () => {
   console.log('Provider service listening on http://localhost:8081')
@@ -29,7 +16,6 @@ describe('Pact Verification', () => {
     let opts = {
       provider: 'Order API',
       providerBaseUrl: 'http://localhost:8081',
-      providerStatesSetupUrl: 'http://localhost:8081/setup',
       pactUrls: [
         path.resolve(process.cwd(), './pacts/order_web-order_api.json'),
       ],
